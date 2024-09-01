bb560f4791035644632bc83e3d1c35e17db729ba
Sidratul Montaha <sonykhan1121@gmail.com>
2024-09-01 12:58:49 +0600

    edge list holding

---
+++ edge_list.cpp
@@ -0,0 +1,19 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+
+    int n ,e;
+    cin>>n>>e;
+    vector<pair<int,int>> ar;
+    while(e--)
+    {
+        int a,b;
+        cin>>a,b;
+        v.push_back(make_pair(a,b));
+        v.push_back({a,b});
+    }
+
+    
+    return 0;
+ }
