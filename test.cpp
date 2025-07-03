00c28af4cdf0496d723689eebd0bc956c74a7ce2
Sidratul Montaha <sonykhan1121@gmail.com>
2025-07-03 14:53:44 +0600

    test commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+
+using namespace std;
+int main() {
+    cout<<"hello world"<<endl;
+    
+    int age;
+    cin>> age;
+    cout << "Your age is: " << age << endl;
+
+    return 0;
+}
